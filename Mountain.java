5356ecc36777ae5e2abe359eeb99ba1a146267dd
apcs <apcs@mvhs-u528p31.dcsdk12.org>
2014-01-07 07:49:21 -0700

    Completed

--- Mountain.java
+++ Mountain.java
@@ -1,3 +1,25 @@
 public class Mountain {
 	
+	public static int getPeakIndex(int[] array){
+		if (array.length<3) {
+			return -1;
+		}
+		for (int i=1; i<array.length-1; i++) {
+			if (array[i-1]<array[i] && array[i+1]<array[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static boolean isMountain(int[] array){
+		int peak = this.getPeakIndex(array);
+		if (peak==-1) {
+			return false;
+		}
+		if (thi.isIncreasing(array,peak)&& this.isDecreasing(array,peak)) {
+			return true;
+		}
+		return false;
+	}
 }
